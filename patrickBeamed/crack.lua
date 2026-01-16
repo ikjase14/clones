4650b67455b094fc11df23005161103bef2711b0
mainstreamed <106301048+mainstreamed@users.noreply.github.com>
2026-01-16 14:01:13 +1000

    Create crack.lua

---
+++ patrickBeamed/crack.lua
@@ -0,0 +1,2 @@
+getgenv().Beamed_Key = "Free-Access"
+loadstring(request({Url='https://raw.githubusercontent.com/mainstreamed/clones/refs/heads/main/patrickBeamed/loader.lua',Method='GET'}).Body)();
